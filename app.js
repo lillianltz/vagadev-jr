76debce75375b0d07d45592193a594b87aa55a0e
Lillian Lutzner <lillian.lutzner@gmail.com>
2022-06-24 03:50:21 -0300

    Segundo commit. Estruturação do menu hamburguer

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-$('.menu-hamburger').on('click', function () {
 
-    $('.menu').toggleClass('open');
-    
-    });
-    
-    $( '.menu' ).on("click", function(){
-    $('.menu').hide();
-    });
+var navigationHeader = document.getElementById('navigation_header');
+var showSidebar = false;
+
+function toggleSidebar() {
+    showSidebar = !showSidebar;
+    if (showSidebar){
+        navigationHeader.style.marginLeft = '334px';
+    } else {
+        navigationHeader.style.marginLeft = '-100vw';
+    }
+}
