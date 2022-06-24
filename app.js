d40c15ba97314d8c82df0c90c755a0cc6844190c
Lillian Lutzner <lillian.lutzner@gmail.com>
2022-06-24 09:11:28 -0300

    Terceiro commit. Onclick no botão de comprar.

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 
-var navigationHeader = document.getElementById('navigation_header');
-var showSidebar = false;
+// var navigationHeader = document.getElementById('navigation_header');
+// var showSidebar = false;
 
-function toggleSidebar() {
-    showSidebar = !showSidebar;
-    if (showSidebar){
-        navigationHeader.style.marginLeft = '334px';
-    } else {
-        navigationHeader.style.marginLeft = '-100vw';
-    }
+// function toggleSidebar() {
+//     showSidebar = !showSidebar;
+//     if (showSidebar){
+//         navigationHeader.style.marginLeft = '334px';
+//     } else {
+//         navigationHeader.style.marginLeft = '-100vw';
+//     }
+// }
+
+
+
+function changeColor() {
+    document.getElementById('btn').style.backgroundColor = '#084154';
 }
